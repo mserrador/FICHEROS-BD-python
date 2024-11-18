8f5e0a2b09c8d1f7ff1c1c1d8bfd7d45ce4410ec
mserrador <5902919@alu.murciaeduca.es>
2024-11-18 19:09:42 +0100

    18/11/24

--- actualizacionConFite.py
+++ actualizacionConFite.py
@@ -1,7 +1,8 @@
 """
 Este fichero se va a encargar de actulizar los registros del fibi que coincidan su campo codigo con los registro de fite que coincidan en el campo codigo,
 para ello el programa lo primero que hace es leer un registro del fite y comparar su campo codigo con los del fibi, si coinciden sustituye el registro 
-que hay en fibi por el de fite
+que hay en fibi por el de fite, ademas en fite hay que modificar el registro acutalizando el campo timestamp cuando se hace una actualizacion del fibi para que quede regisrado en fite
+
 """
 # importamos las librerias necesarias
 import struct
@@ -20,13 +21,15 @@ s = struct.Struct("I 15s 50s 9s I f f 1s 18s 18s")
 # vemos cuantos registros tiene el fichero binario
 ficheroB = os.stat("fibi")
 bytesB = ficheroB.st_size
-registrosB =  int(bytes/129)
+registrosB =  int(bytesB/129)
 
 
 # vemos cuantos registros tiene el fichero de texto
 ficheroT = os.stat("fite")
 bytesT = ficheroT.st_size
-registrosT =  int(bytes/34)
+registrosT =  int(bytesT/34)
+
+contador = 0
 
 for i in range(registrosT):
     # leo un registro
@@ -43,49 +46,40 @@ for i in range(registrosT):
 
     # creamos la variable que va a guardar el codigo de fibi
     codigoB = 0
+    
+    fibi.seek(0)
+    for j in range(registrosB):
+        # hago que el puntero lea un registro entero
+        registroB = s.unpack(fibi.read(129))
 
-    nomas = 0
+        # saco el campo codigo del fichero binario
+        codigoB = registroB[0]
 
-    if nomas < bytesB:
-        while codigoT != codigoB:
-            # hago que el puntero lea un registro entero
-            registro = s.unpack(fibi.read(129))
-            nomas = nomas+129
+        if codigoT == codigoB:
+            # aqui hemos encontrado 2 registros que coinciden en el campo codigo
+            # hay que sustituir el campo del dni en fibi
+            
+            # guardamos en una variable el dni de el registro de fite
+            dniT = registroT[5]+registroT[6]+registroT[7]+registroT[8]+registroT[9]+registroT[10]+registroT[11]+registroT[12]+registroT[13]
 
-            # saco el campo codigo del fichero binario
-            codigoB = registro[0]
+            # creamos el nuevo registro a guardar en fibi, pero lleva el dni del fite
+            fechaActual = datetime.now()
+            timestampSituacion= fechaActual.strftime("%Y-%m-%d %H:%M:%S")
+            registroNuevo=s.pack(registroB[0],registroB[1],registroB[2],dniT.encode("UTF-8"),registroB[4],registroB[5],registroB[6],"m".encode("UTF-8"),timestampSituacion.encode("UTF-8"),registroB[9])
 
-            if codigoT == codigoB:
-                print('coincidencia encontrada')
+            # tambien creamos el registro que se va a guardar en fite sustituyendo el campo timestamp
+            cadena = "{0:05}{1:9s}{2:19s}{c}".format(codigoT,dniT,timestampSituacion,c="\n")
+            fite.seek(i*34)
+            fite.write(cadena)
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+            # ajustamos el puntero para que sobreescriba el registro previeo
+            fibi.seek(j*129)
+            # sobreescribimos el registro
+            fibi.write(registroNuevo)
+            contador +=1
 
 
-
-
-
-
-
-
-
-
+print("se han modificado",contador,"clientes")           
 
 # cerramos los ficheros
 fibi.close()
